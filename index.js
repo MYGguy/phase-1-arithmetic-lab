379851a2d12bbbab5c90f1019eb716abc2a239e6
Moshe Goldstein <mygguy613@gmail.com>
2024-09-09 17:39:19 -0400

    cl

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
 
-// Write your code here
+// console.log(multiply(4, 15.5));
+
+const num1 = 4;
+const num2 = 15.5;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() + 1);
+
+const num3 = 59;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(3, 5, 20, 6, 17);
